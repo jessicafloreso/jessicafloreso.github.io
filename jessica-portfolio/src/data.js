e61eafda4334a0308cdefae1eff31d1712887cd6
jessicafloreso <jessica.floresolguin@gmail.com>
2023-09-11 01:40:52 -0400

    commit

--- jessica-portfolio/src/data.js
+++ jessica-portfolio/src/data.js
@@ -12,7 +12,7 @@ export const projects = [
       subtitle: "Tangible UI - Wrist Wearable",
       description:
         "Ardunio micro-computer programmed to allow user to check heart bpm in order to help regulate emotions. The heart sensor is connected to the neo-pixels to display user's emotional state via heart BPM",
-      image: "./assets/img/HeartBeatz.jpg",
+      image: "../HeartBeatz.jpg",
       link: "https://anainamalik172.wixsite.com/mysite",
     },
     {
@@ -20,7 +20,7 @@ export const projects = [
       subtitle: "Spring Console Application",
       description:
         "Keep track of movies you've watched or want to watch, then rate them. Check out what other people rated too! ",
-      image: "./project-3.gif",
+      image: "../movie.jpeg",
       link: "https://github.com/jessicafloreso/Jump-Plus-Projects/tree/main/MovieRatingConsoleApp",
     },
     {
@@ -28,7 +28,7 @@ export const projects = [
       subtitle: "Python + MongoDB + React App",
       description:
         "Want to get trivial? Check out this fun quiz app. Users are able to add questions and take their knowledge to the test by taking a randomized fun quiz!",
-      image: "./project-4.gif",
+      image: "../quiz.png",
       link: "https://github.com/mandy2324/Fun-Quiz-Platform-Backend",
     },
     {
@@ -36,7 +36,7 @@ export const projects = [
         subtitle: "Figma UI/UX Design Project",
         description:
           "Targeting users from varying stress backgrounds, Cocoon is a web application desgined to help First-Gen Low-Income Wellesley students navigate their emotions, mindfulnees, and goals through a non-invasive, centralized application.",
-        image: "./project-4.gif",
+        image: "../cocoon.jpg",
         link: "https://github.com/mandy2324/Fun-Quiz-Platform-Backend",
       },
   ];
